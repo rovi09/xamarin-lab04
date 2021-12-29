b59c6219f32951249d17b87d586dd406c959fe28
Esteban Solano <stvansolano>
2021-12-28 23:21:39 -0600

    Sesion-03 - Completa

--- material/MobileApp/MobileApp/App.cs
+++ material/MobileApp/MobileApp/App.cs
@@ -7,79 +7,29 @@ namespace MobileApp
 	{
 		public App()
 		{
-			MainPage = new ContentPage { 
-				Content = new Label {
-					Text = "Hello, World!, clase xamarin",
-					VerticalOptions = LayoutOptions.Center, 
-					HorizontalOptions = LayoutOptions.Center
-				} 
+			var label = new Label
+			{
+				Text = "Hello, World!",
+				FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label)),
+			};
+
+			var button = new Button { Text = "Click me" };
+			int i = 0;
+			button.Clicked += (s, e) => {
+				button.Text = "Hello, World! " + i++;
+			};
+
+			MainPage = new ContentPage
+			{
+				BackgroundColor = Color.Transparent,
+				Content = new StackLayout
+				{
+					BackgroundColor = Color.Transparent,
+					VerticalOptions = LayoutOptions.Center,
+					HorizontalOptions = LayoutOptions.Center,
+					Children = { label, button }
+				}
 			};
 		}
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/**** #2
-			var label = new Label { Text = "Hello, World!" };
-			var button = new Button { Text = "Click me!" };
-
-			MainPage = new ContentPage {
-				Content = new StackLayout {
-					Children = { label, button },
-					HorizontalOptions = LayoutOptions.Center,
-					VerticalOptions = LayoutOptions.Center,
-				}
-			};
-
-			var i = 0;
-			button.Clicked += (s, e) => { label.Text = "Hello, World! " + (i = i + 1); };				 
-*/
-
-/**** #3
-var page = new ContentPage
-{
-	Content = new Label
-	{
-		Text = "Hello, World!",
-		VerticalOptions = LayoutOptions.Center,
-		HorizontalOptions = LayoutOptions.Center,
-	},
-	Title = "Welcome"
-};
-
-View titleView = new StackLayout {
-	HorizontalOptions = LayoutOptions.Center,
-	VerticalOptions = LayoutOptions.Center,
-	Children = {
-		new Label { Text = page.Title, TextColor = Color.White, }
-	},
-};
-NavigationPage.SetTitleView(page, titleView);
-
-MainPage = new NavigationPage(page) {
-	BarBackgroundColor = Color.FromHex("#1976D2"),
-	BarTextColor = Color.White,
-};*/
