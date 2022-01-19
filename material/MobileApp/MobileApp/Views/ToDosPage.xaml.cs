c5c90aea379296d0cf6c20ea8e7501c5134230b7
Esteban Solano <stvansolano>
2022-01-18 22:13:33 -0600

    Sesion-05 completa

--- material/MobileApp/MobileApp/Views/ToDosPage.xaml.cs
+++ material/MobileApp/MobileApp/Views/ToDosPage.xaml.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -9,7 +13,9 @@ namespace MobileApp.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ToDosPage : ContentPage
     {
-        public ObservableCollection<object> ToDos { get; set; } = new ObservableCollection<object>();
+        public ObservableCollection<ToDoItem> ToDos { get; set; } = new ObservableCollection<ToDoItem>();
+
+        public ToDoService Service { get; } = new ToDoService();
 
         public ToDosPage()
         {
@@ -17,28 +23,54 @@ namespace MobileApp.Views
 
             InitializeComponent();
 
-            ToDos.Add("Buy cookies");
-            ToDos.Add("test 2");
-
             BindableLayout.SetItemsSource(ItemsContainer, ToDos);
 
-            Root.Children.Add(new Button { 
-                Text = "Add", 
+            Root.Children.Add(new Button
+            {
+                Text = "Add",
                 Command = new Command(() => AddItem()),
                 //VerticalOptions = 
             });
         }
 
-        protected override void OnAppearing()
+        protected async override void OnAppearing()
         {
             base.OnAppearing();
 
-            ToDos.Add(DateTime.Now);
+            ToDoItem[] result = await Service.GetToDos();
+
+            Debug.WriteLine(result);
+
+            foreach (var item in result)
+            {
+                ToDos.Add(item);
+            }
         }
 
         private void AddItem()
         {
-            ToDos.Add(DateTime.Now);
+            ToDos.Add(new ToDoItem { Title = DateTime.Now.ToString() });
         }
+    }
+    public class ToDoService
+    {
+        public async Task<ToDoItem[]> GetToDos()
+        {
+            var http = new HttpClient();
+
+            http.BaseAddress = new Uri("https://024b-201-202-14-8.ngrok.io");
+
+            string json = await http.GetStringAsync("/api/todos");
+
+            var result = JsonConvert.DeserializeObject<ToDoItem[]>(json);
+
+            return result;
+        }
+    }
+    public class ToDoItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public bool IsComplete { get; set; }
     }
 }
